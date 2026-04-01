850f342a70d47471cee00d5ac04e7f930bf908e3
Sierra <ziacreatesideas@gmail.com>
2026-04-01 11:24:31 -0600

    Book Theme

--- doc/conf.py
+++ doc/conf.py
@@ -64,7 +64,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Sphinx Template'
+project = 'Study Isaac RL'
 copyright = '2026'
 author = 'Zia'
 
@@ -102,9 +102,12 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+
+html_theme = "sphinx_book_theme"
+
 # html_theme = 'sphinx_rtd_theme'
 
-html_theme = "pydata_sphinx_theme"
+# html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
